d46df7c29cf6759a6a8da509a5690e91160ee121
burnash <anton.burnashev@gmail.com>
2016-08-04 20:46:22 +0300

    move header info from api.py, update authors

--- pyneurovault_upload/__init__.py
+++ pyneurovault_upload/__init__.py
@@ -1,3 +1,19 @@
+"""
+    pyneurovault_upload
+    ~~~~~~~~~~~~~~~~~~~
+
+    A client for interfacing with http://neurovault.org API
+
+
+    :copyright: (c) 2016 by Luke Chang.
+    :license: MIT, see LICENSE for more details.
+"""
+
 API_BASE_URL = 'http://neurovault.org/api/'
 
-from session import Client
+from client import Client
+
+__all__ = ['Client']
+
+__author__ = ['Luke Chang', 'Anton Burnashev']
+__license__ = 'MIT'
