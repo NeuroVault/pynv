a8e15bb74c1de2189fe92b3f16099008c57f4841
burnash <anton.burnashev@gmail.com>
2016-08-05 19:25:57 +0300

    test create collection

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -1,6 +1,8 @@
 import pytest
 
-from pyneurovault_upload.exceptions import ValidationError, APIError
+from pyneurovault_upload.exceptions import (
+    APIError, AuthenticationError, ValidationError
+)
 
 
 def test_create_collection_for_anonymous_user(anonymous_client, recorder):
@@ -19,14 +21,20 @@ def test_create_collection_with_missing_fields(client, recorder):
 
 def test_create_collection_with_blank_name(client, recorder):
     with pytest.raises(ValidationError) as excinfo:
-        collection = client.create_collection('')
-        print collection
+        client.create_collection('')
 
     assert excinfo.value.errors == {'name': ['This field may not be blank.']}
 
 
-def test_create_collection():
-    pass
+def test_create_collection(client, recorder):
+    with recorder.use_cassette('create_collection'):
+        collection = client.create_collection('NV Test Collection')
+
+    subset_dict = {
+        'name': 'NV Test Collection'
+    }
+
+    assert subset_dict.viewitems() <= collection.viewitems()
 
 
 def test_read_collection():
@@ -41,8 +49,10 @@ def test_update_collection():
     pass
 
 
-def test_delete_collection_for_anonymous_user():
-    pass
+def test_delete_collection_for_anonymous_user(anonymous_client, recorder):
+    with recorder.use_cassette('delete_collection'):
+        with pytest.raises(AuthenticationError):
+            anonymous_client.delete_collection(1610)
 
 
 def test_delete_collection(client, recorder):
@@ -62,5 +72,7 @@ def test_my_collections(client, recorder):
         assert len(collection['name']) > 0
 
 
-def test_my_collections_for_anonymous_user():
-    pass
+def test_my_collections_for_anonymous_user(anonymous_client, recorder):
+    with recorder.use_cassette('my_collections_anonymous'):
+        with pytest.raises(AuthenticationError):
+            anonymous_client.my_collections()
