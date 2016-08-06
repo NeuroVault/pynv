30c90d29fe09498e4b54c3322de6502e69f0af1f
burnash <anton.burnashev@gmail.com>
2016-08-06 18:27:57 +0300

    better name

--- pyneurovault_upload/client.py
+++ pyneurovault_upload/client.py
@@ -61,8 +61,8 @@ class Client(object):
     def my_collections(self):
         return self.request('get', 'my_collections').json()
 
-    def add_image(self, collection_id, file_object_or_string, **data):
-        files = {'file': open(file_object_or_string, 'rb')}
+    def add_image(self, collection_id, file, **data):
+        files = {'file': open(file, 'rb')}
 
         return self.request(
             'post',
